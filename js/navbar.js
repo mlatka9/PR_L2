e1783f12504b5d8533c73ca33f3c364ac79f2b36
Mateusz Łątka <mateusz.latka9@gmail.com>
2021-03-19 10:40:56 +0100

    add navbar

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><ul><li><a href="index.html">główna</a></li><li><a href="contact.html">Kontakt</a></li></ul></nav>'
+
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
